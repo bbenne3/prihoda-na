c84f87d1d1a98b79330431b76281abdfa8718558
bbenne3 <bennettb0677@gmail.com>
2023-10-07 10:59:19 -0400

    Updating contact page with job details

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -1,12 +1,23 @@
-import { Text, SafeAreaView, View, ImageBackground, Image } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  Pressable,
+  View,
+  ImageBackground,
+  Image,
+  Dimensions,
+} from "react-native";
 import { A } from "@expo/html-elements";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { FC, PropsWithChildren, useRef, useState } from "react";
 
 const resolved = Image.resolveAssetSource(
   require("../../assets/logos/Prihoda3.png")
 );
 
 export const Contact = () => {
+  const [showing, showOptions] = useState<PropsWithChildren["children"]>();
+
   return (
     <SafeAreaView style={{ flex: 1, display: "flex" }}>
       <View
@@ -36,47 +47,113 @@ export const Contact = () => {
               textAlign: "center",
             }}
           >
-            Thank You For Your Interest
+            Job Details
           </Text>
-          <View
-            style={{ paddingTop: 48 }}
-            aria-role="presentation"
-          />
-          <Text style={{ fontSize: 18, lineHeight: 24 }}>
-            To find out more about Prihoda's Tailor-Made Air Dispersion Systems
-            please visit us at{" "}
-            <A href="https://prihodafabricduct.com" style={{color: "#01ad7f"}}>
-              https://prihodafabricduct.com <FontAwesome5 name="external-link-alt" size={16} color="#01ad7f" />
-            </A>
-          </Text>
-          <View style={{ paddingTop: 18 }} aria-role="presentation" />
-          <Text style={{ fontSize: 18, lineHeight: 24 }}>
-            More app features to come.
-          </Text>
+          <View style={{marginTop: 16}}>
+            <JobDetail type="Suspension Method" onClick={showOptions}>
+              <SectionTitle>Suspension Method</SectionTitle>
+              <Text>Track</Text>
+              <Text>Cable</Text>
+            </JobDetail>
+            <JobDetail type="Material Type" onClick={showOptions}></JobDetail>
+            <JobDetail type="Dispersion Type" onClick={showOptions}>
+              <SectionTitle>Dispersion Type</SectionTitle>
+              <Text>Perforations (10-30 ft heating and cooling)</Text>
+              <Text>Nozzles (30+ feet)</Text>
+              <Text>Large nozzles (45+ feet)</Text>
+            </JobDetail>
+            <JobDetail type="Custom Art" onClick={showOptions}>
+              <SectionTitle>Custom Art</SectionTitle>
+              <Text>Yes</Text>
+              <Text>No</Text>
+            </JobDetail>
+            <JobDetail type="CFM" onClick={showOptions}>
+              <SectionTitle>CFM</SectionTitle>
+            </JobDetail>
+          </View>
           <View style={{ paddingTop: 48, flex: 1 }} aria-role="presentation" />
           <A
             href="https://prihodafabricduct.com/contact/"
             style={{
               backgroundColor: "#0f7ba5",
-              padding: 24,
+              paddingVertical: 16,
+              paddingHorizontal: 22,
               maxWidth: 400,
               color: "#ffffff",
-              display: 'flex',
+              display: "flex",
               textAlign: "center",
               alignSelf: "center",
               justifyContent: "flex-end",
-              lineHeight: 32,
+              lineHeight: 28,
               fontWeight: "700",
-              fontSize: 24,
+              fontSize: 22,
             }}
           >
-            Contact Us{" "}
+            Get Quote Selection{" "}
             <FontAwesome5 name="external-link-alt" size={16} color="#FFFFFF" />
           </A>
           <View style={{ paddingBottom: 48 }} aria-role="presentation" />
-          <Text style={{textAlign: 'center'}}> &#169; Prihoda, NA | All Rights Reserved</Text>
+          <Text style={{ textAlign: "center" }}>
+            {" "}
+            &#169; Prihoda, NA | All Rights Reserved
+          </Text>
+          <View style={{ paddingBottom: 16 }} aria-role="presentation" />
+          <A
+            href="https://prihodafabricduct.com"
+            style={{ color: "#01ad7f", textAlign: "center" }}
+          >
+            https://prihodafabricduct.com{" "}
+            <FontAwesome5 name="external-link-alt" size={16} color="#01ad7f" />
+          </A>
         </ImageBackground>
       </View>
+      {!!showing && (
+        <View
+          style={{
+            backgroundColor: "#FFFFFF",
+            position: "absolute",
+            alignSelf: "center",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 1,
+          }}
+        >
+          <Pressable style={{ position: "absolute", top: 16, right: 16, zIndex: 1 }} onPress={() => showOptions(false)}>
+            <Ionicons name="close-sharp" size={24} color="black" />
+          </Pressable>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 16,
+              padding: 24,
+              backgroundColor: "#01ad7f33",
+            }}
+          >
+            {showing}
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
+
+const JobDetail: FC<
+  PropsWithChildren<{
+    type: string;
+    onClick: (c: PropsWithChildren["children"]) => void;
+  }>
+> = ({ type, onClick, children }) => {
+  return (
+    <View style={{ paddingVertical: 6, display: "flex", gap: 8 }}>
+      <Pressable onPress={() => onClick(children)}>
+        <SectionTitle>{type}</SectionTitle>
+      </Pressable>
+    </View>
+  );
+};
+
+const SectionTitle = (props: PropsWithChildren) => (
+  <Text style={{fontSize: 20, fontWeight: "500"}}>{props.children}</Text>
+)
