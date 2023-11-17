149e5502318335aa42ba66a9bf58ab03460ed791
bbenne3 <bennettb0677@gmail.com>
2023-11-17 18:08:09 -0500

    More updates  - update contact button verbiage  - move website up / more prominent  - update app icons

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -1,26 +1,16 @@
 import {
-  Animated,
   Text,
   SafeAreaView,
   Pressable,
   View,
-  Image,
   Alert,
   StyleSheet,
   Platform,
   StatusBar,
 } from "react-native";
 import { A } from "@expo/html-elements";
-import { FontAwesome5, Ionicons } from "@expo/vector-icons";
-import {
-  Children,
-  FC,
-  PropsWithChildren,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { useCallback } from "react";
 import * as MailComposer from "expo-mail-composer";
 import SplashLogo from "../components/icons/SplashLogo";
 
@@ -107,7 +97,7 @@ export const Contact = ({ navigation, route }) => {
             style={{
               backgroundColor: "#0f7ba5",
               padding: 24,
-              maxWidth: 400,
+              maxWidth: 500,
               gap: 8,
               display: "flex",
               flexDirection: "row",
@@ -125,101 +115,31 @@ export const Contact = ({ navigation, route }) => {
                 fontSize: 22,
               }}
             >
-              Get Quote Selection{" "}
+              Product Selection/Quote{" "}
             </Text>
             <FontAwesome5 name="external-link-alt" size={16} color="#FFFFFF" />
           </Pressable>
+          <View style={{ paddingTop: 48 }} aria-role="presentation" />
+
+          <A
+            href="https://prihodafabricduct.com"
+            style={{ color: "#01ad7f", textAlign: "center", fontSize: 22 }}
+          >
+            prihodafabricduct.com{" "}
+            <FontAwesome5 name="external-link-alt" size={16} color="#01ad7f" />
+          </A>
           {/* <View style={{ paddingBottom: 16 }} aria-role="presentation" /> */}
         </View>
         <View style={{ paddingTop: 48, flex: 1 }} aria-role="presentation" />
-        <View>
+        <View style={{ paddingBottom: 32 }}>
           <Text style={{ textAlign: "center" }}>
             {" "}
             &#169; Prihoda, NA | All Rights Reserved
           </Text>
           <View style={{ paddingBottom: 16 }} aria-role="presentation" />
-          <A
-            href="https:prihodafabricduct.com"
-            style={{ color: "#01ad7f", textAlign: "center" }}
-          >
-            https:prihodafabricduct.com{" "}
-            <FontAwesome5 name="external-link-alt" size={16} color="#01ad7f" />
-          </A>
         </View>
       </View>
     </SafeAreaView>
-    // <SafeAreaView
-    //   style={[
-    //     {
-    //       display: "flex",
-    //       flex: 1,
-    //       alignItems: "center",
-    //       justifyContent: "center",
-    //       gap: 64,
-    //     },
-    //     styles.safeArea,
-    //   ]}
-    // >
-    //   <View style={{ width: "100%", display: "flex", alignItems: "center" }}>
-    //     <Image
-    //       source={{ uri: logoSource.uri }}
-    //       width={300}
-    //       style={{ aspectRatio: "7/2.8", marginLeft: !isAndroid ? -20 : 0 }}
-    //     />
-    //   </View>
-    //   <View
-    //     style={{
-    //       padding: 24,
-    //       display: "flex",
-    //       alignItems: "center",
-    //       justifyContent: "center",
-    //     }}
-    //   >
-    //     <Pressable
-    //       onPress={() => {
-    //         openEmail();
-    //       }}
-    //       style={{
-    //         backgroundColor: "#0f7ba5",
-    //         padding: 24,
-    //         maxWidth: 400,
-    //         gap: 8,
-    //         display: "flex",
-    //         flexDirection: "row",
-    //         alignSelf: "center",
-    //         alignItems: "center",
-    //         justifyContent: "flex-end",
-    //       }}
-    //     >
-    //       <Text
-    //         style={{
-    //           color: "#FFFFFF",
-    //           textAlign: "center",
-    //           lineHeight: 28,
-    //           fontWeight: "700",
-    //           fontSize: 22,
-    //         }}
-    //       >
-    //         Get Quote Selection{" "}
-    //       </Text>
-    //       <FontAwesome5 name="external-link-alt" size={16} color="#FFFFFF" />
-    //     </Pressable>
-    //   </View>
-    //   <View style={{ paddingTop: 48, flex: 1 }} aria-role="presentation" />
-    //   <View style={{ paddingBottom: 48 }} aria-role="presentation" />
-    //   <Text style={{ textAlign: "center" }}>
-    //     {" "}
-    //     &#169; Prihoda, NA | All Rights Reserved
-    //   </Text>
-    //   <View style={{ paddingBottom: 16 }} aria-role="presentation" />
-    //   <A
-    //     href="https://prihodafabricduct.com"
-    //     style={{ color: "#01ad7f", textAlign: "center" }}
-    //   >
-    //     https://prihodafabricduct.com{" "}
-    //     <FontAwesome5 name="external-link-alt" size={16} color="#01ad7f" />
-    //   </A>
-    // </SafeAreaView>
   );
 };
 
